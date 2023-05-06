935e43b22dc5e246f31b583e58970b60ef50f05e
diuca-x <126406046+diuca-x@users.noreply.github.com>
2023-05-06 13:06:11 +0000

    formated the timer

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,18 +8,26 @@ import Timer from "./timer";
 const Home = ({seconds}) => {
 	const icon = {
 		color: "color: #ffffff"
+
+	}
+	const box_styles = {
+		height: "9vw",
+		width: "6vw"
+	}
+	const icon_style = {
+		fontSize: "3vw"
 	}
 	return (
 		<>
 			<div className="text-center container">
-				<div className="row justify-content-center bg-dark">
-					<div className="col-2 border  text-light"><i class="far fa-clock fa-lg"></i></div>
-					<div className="col-1 border  text-light"><Timer seconds={seconds} interval={6}/></div>
-					<div className="col-1 border  text-light"><Timer seconds={seconds} interval={5}/></div>
-					<div className="col-1 border  text-light"><Timer seconds={seconds} interval={4}/></div>
-					<div className="col-1 border  text-light"><Timer seconds={seconds} interval={3}/></div>
-					<div className="col-1 border  text-light"><Timer seconds={seconds} interval={2}/></div>
-					<div className="col-1 border  text-light"><Timer seconds={seconds} interval={1}/></div>
+				<div className="row justify-content-center align-items-center bg-black rounded mt-5 ">
+					<div className="col-2  text-light d-flex justify-content-end me-5"><div className="d-flex bg-dark  rounded align-items-center justify-content-center my-2 " style={box_styles}><i className="far fa-clock fa-3x " style={icon_style}></i></div></div>
+					<div className="col-1  text-light"><div className="d-flex  bg-dark  rounded align-items-center justify-content-center my-2" style={box_styles}><Timer seconds={seconds} interval={6}/></div></div>
+					<div className="col-1  text-light"><div className="d-flex  bg-dark  rounded align-items-center justify-content-center my-2" style={box_styles}><Timer seconds={seconds} interval={5}/></div></div>
+					<div className="col-1  text-light"><div className="d-flex  bg-dark  rounded align-items-center justify-content-center my-2" style={box_styles}><Timer seconds={seconds} interval={4}/></div></div>
+					<div className="col-1  text-light"><div className="d-flex  bg-dark  rounded align-items-center justify-content-center my-2" style={box_styles}><Timer seconds={seconds} interval={3}/></div></div>
+					<div className="col-1  text-light"><div className="d-flex  bg-dark  rounded align-items-center justify-content-center my-2" style={box_styles}><Timer seconds={seconds} interval={2}/></div></div>
+					<div className="col-1  text-light"><div className="d-flex  bg-dark  rounded align-items-center justify-content-center my-2" style={box_styles}><Timer seconds={seconds} interval={1}/></div></div>
 				</div>
 			</div>
 		</>
