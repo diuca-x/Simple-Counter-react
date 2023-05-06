a5602d29f094b617b84049a30722da5e56a6bfbd
diuca-x <126406046+diuca-x@users.noreply.github.com>
2023-05-06 13:23:14 +0000

    reverse counter in process.

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Prehome/>,document.querySelector("#app"))
+
 
 let counter = 0
 setInterval(() => {
